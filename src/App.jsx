ec229aceb285c96af0e7ef67afad69d30c7ff671
Arrafi2644 <raffian2644@gmail.com>
2025-03-09 11:05:07 +0600

    Complete basic setup for JobNest

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-3xl font-bold text-center mt-10'>JobNest</h2>
+      <button className="btn text-center mx-auto">Start Now</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className='min-h-screen'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+        
+    );
+};
+
+export default MainLayout;
